3c99cf9a11572014ce8a751c1191c3807c67439d
Yuji Kosugi <yuji.kosugi@gmail.com>
2025-01-31 17:40:46 +0900

    Update types of User properties

--- src/lib/interfaces/user.ts
+++ src/lib/interfaces/user.ts
@@ -1,11 +1,11 @@
 export interface User {
-  first_name: string;
-  last_name: string;
+  first_name: string | null;
+  last_name: string | null;
   email: string;
   personal_name?: string | null;
   uid: string;
-  image1: string;
-  create_date: any;
+  image1: string | null;
+  create_date: number;
   reset_password_email_sent?: boolean;
   custom_fields?: any;
 }
