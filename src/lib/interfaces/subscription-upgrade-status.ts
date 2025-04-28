545e66aeb1176ecd0cd01e9465fba8a5006c113b
gtakat <gtakat@gmail.com>
2025-04-28 13:42:05 +0900

    Change change_date and create_date in SubscriptionUpgradeStatus interface to a nullable field

--- src/lib/interfaces/subscription-upgrade-status.ts
+++ src/lib/interfaces/subscription-upgrade-status.ts
@@ -10,9 +10,9 @@ export interface SubscriptionUpgradeStatus {
   to_term_name: string;
   from_term_id: string;
   to_term_id: string;
-  change_date: string;
-  create_date_from: string;
-  create_date_to: string;
+  change_date: string | null;
+  create_date_from: string | null;
+  create_date_to: string | null;
   billing_plan_to: string;
   billing_plan_from: string;
   status: SubscriptionUpgradeStatusFlag;
